0286f56e0517014599ee629332df708efc099fbf
Cris Kelly <cris.kelly@gmail.com>
2013-03-18 16:40:51 -0700

    Added jquery tabs under Today's Posts

--- app.rb
+++ app.rb
@@ -1,6 +1,23 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'sinatra/json'
+# require './lib/freecycle_mail'
+# require 'json'
+
+# get '/' do
+#   $addresses = []
+#   $addressBook = []
+#   subjects = recent_offers()
+
+#   subjects.each do |subject|
+#     $address << make_offer_data(subject)
+#   end
+#   $addresses.reverse!
+#   for i in 0..4
+#     $addressBook[i] = []
+#   end
+# end
+
 
 get '/' do
   erb :index
--- lib/freecycle_mail.rb
+++ lib/freecycle_mail.rb
@@ -72,6 +72,4 @@ def make_recent_offers_web_data
   recent_offer_places().to_json
 end
 
-hash.new[subject]
-
 # FIXME/TODO [wc 2013-03-14] Make a sensisible API for this.
